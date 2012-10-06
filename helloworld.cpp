fa99ca40fe61419e71652270aa375523cb57652c
wddqing <wddqing@gmail.com>
2012-10-06 16:36:06 +0800

    a helloworld application for test

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+/**********************************************************
+*
+*	@function: This is just a helloworld application!
+*
+*	@time:2012-10-06
+*
+***********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "hello,world!" << endl;
+	return 0;
+}
